48aa4d0f83032d73793b8b24e8a284e2cbad9cf4
Agustín Castino <aguscastino01@gmail.com>
2024-10-30 19:47:57 -0300

    initial commit

---
+++ src/main/java/com/uade/BBDD2/Bbdd2Application.java
@@ -0,0 +1,13 @@
+package com.uade.BBDD2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bbdd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bbdd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uade/BBDD2/Bbdd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.BBDD2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bbdd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
