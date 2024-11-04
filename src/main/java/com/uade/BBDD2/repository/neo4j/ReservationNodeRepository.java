0cee4f084ef5c754d53408b8b7b8e058993e8a0e
Agustín Castino <aguscastino01@gmail.com>
2024-11-03 23:26:25 -0300

    Se crean los repository

--- src/main/java/com/uade/BBDD2/repository/neo4j/ReservationNodeRepository.java
+++ src/main/java/com/uade/BBDD2/repository/neo4j/ReservationNodeRepository.java
@@ -1,7 +1,6 @@
 package com.uade.BBDD2.repository.neo4j;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
-
 import com.uade.BBDD2.model.neo4j.ReservationNode;
 
 public interface ReservationNodeRepository extends Neo4jRepository<ReservationNode, String>{
